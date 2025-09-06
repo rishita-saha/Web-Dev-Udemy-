48af54ce0963eccf90c08a934028030bb03c922b
rish-gitcode <rishitasaha291@gmail.com>
2025-09-07 02:22:51 +0530

    Section 10:operators in js

---
+++ JS/part1/assignment.js
@@ -0,0 +1,8 @@
+let score = 10 + 11
+
+let num1 = 12
+
+num1 += 4; // num1 = num1 + 4
+
+num1 -= 1; // num1 = num1 - 1
+console.log(num1);
---
+++ JS/part1/operations.js
@@ -0,0 +1,32 @@
+let score = 102
+let bonus = 25
+
+let total = score + bonus
+
+
+let add = 2 + 3
+let sub = 9 - 2
+let mul = 3 * 5
+let div = 8 / 4
+let remainder = 9 % 4
+let expo = 2 ** 4
+
+let myscore = 110
+myscore++
+
+let credits = 44
+credits--
+
+//comparision operators
+
+let num1 = 3
+let num2 = 3
+let num3 = 6
+let num4 = "6"
+
+console.log(num1 == num2)
+console.log(num1 == num3);
+console.log(num1 != num3);
+console.log(num3 == num4);
+console.log(num3 === num4);
+
