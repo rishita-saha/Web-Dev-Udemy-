7d0d0ed602e149d0774c16154ca54794abf49cf1
rish-gitcode <rishitasaha291@gmail.com>
2025-10-24 21:48:02 +0530

    Section 10: Non-premitives in js

---
+++ JS/part2/nonPrimitives.js
@@ -0,0 +1,30 @@
+//objects
+const user ={ 
+    "first Name": "Titli",
+    isLoggedin: true,
+};
+
+//user.firstName = "Rishita";
+user.lastName = "Saha";
+
+// console.log(user["first Name"]);
+// console.log(user.lastName);
+// console.log(user);
+// console.log(typeof user);
+
+let today = new Date();
+// console.log(today.getDate());
+// console.log(typeof today);
+
+//Array
+let user2 = ["Rishita", true];
+//console.log(user2[0]);
+
+let value = true;
+console.log(value +1);
+let v = "2abc";
+console.log(Number(v));
+console.log(typeof Number(v));
+console.log(Number(null));
+console.log(Number(undefined));
+
