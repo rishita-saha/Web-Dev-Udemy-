4855a87e186fce347b4a47e6d9e31b986dbedc8e
rish-gitcode <rishitasaha291@gmail.com>
2025-10-25 22:21:20 +0530

    Section 10: conditions in js

---
+++ JS/part3/conditions.js
@@ -0,0 +1,53 @@
+//check if a number is greater than another number: 
+
+let n1=5;
+let n2= 9;
+// console.log("I am a regular upper code");
+
+// if(n1>n2){
+//     console.log("n1 is greater than n2");
+// }
+// else{
+//     console.log("NOPE! n2 is greater than n1");
+// }
+// console.log("I am a regular bottom code");
+
+//check if a string is equal to another string:
+
+let user1 = "Titli";
+let user2 = "Titli";
+
+// if(user1 == user2){
+//     console.log("Both users are same\n Please pick another name")
+// }
+// else{
+//     console.log("You can pick this username");
+// }
+
+//check if a variable is number or not:
+
+let value = [];
+/*if(typeof value == 'string'){
+    console.log("Yes, This is a number");
+}
+else{
+    console.log("No, This is not a number");
+}*/
+
+//Checking boolean value is true or false
+let isTeaReady = false;
+if(isTeaReady){
+    console.log("Tea is Ready");
+
+}else{
+//console.log("Tea is not ready")
+}
+
+//ckecking array is empty or not:
+let items = ["i1","i2"];
+console.log(items.length);
+if(items.length == 0){
+    console.log("Array is empty");
+}else{
+    console.log("Array is not empty")
+}
