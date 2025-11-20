5e4583e9bdfb020797f8ece5e1a213620cee98d7
Rishita Saha <rishitasaha291@gmail.com>
2025-11-20 22:37:41 +0530

    Section 12:JS-OOPS objects completed

--- JS/part6/oop-master.js
+++ JS/part6/oop-master.js
@@ -30,4 +30,30 @@ Array.prototype.titli = function(){
 }
 
 let myArray = [1,2,3];
-console.log(myArray.titli());
+// console.log(myArray.titli());
+
+let myNewArray = [1,2,3,4,5,6];
+// console.log(myNewArray.titli());
+
+class Vehicle {
+    constructor(make,model){
+        this.make = make;
+        this.model = model;
+    }
+    start(){
+        return `${this.model} is a car from ${this.make}`;
+    }
+}
+
+class Car extends Vehicle{
+    drive(){
+    return `${this.make} : This is an inheritance example`;
+    }
+}
+
+let myCar = new Car("Toyota","Corola");
+// console.log(myCar.start());
+// console.log(myCar.drive());
+
+let veh = new Vehicle("Toyota","Corola");
+console.log(veh.make);
