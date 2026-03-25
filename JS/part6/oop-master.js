360c70c1f2202ade14e98c1f8cb5b2ebf9c52b14
Rishita Saha <rishitasaha291@gmail.com>
2026-03-26 03:59:54 +0530

    start learing react

--- JS/part6/oop-master.js
+++ JS/part6/oop-master.js
@@ -56,4 +56,105 @@ let myCar = new Car("Toyota","Corola");
 // console.log(myCar.drive());
 
 let veh = new Vehicle("Toyota","Corola");
-console.log(veh.make);
+// console.log(veh.make);
+
+//Encapsulation
+
+class BankAccount{
+    #balance = 0 ;
+
+    deposite(amount){
+        this.#balance += amount
+        return this.#balance;
+    }
+
+    getBalance(){
+        return `${this.#balance}`
+    }
+}
+
+let account = new BankAccount();
+// console.log(account.getBalance());
+
+//Abstraction
+
+class CoffeeMachine{
+    start(){
+        //call DB
+        //filter value
+        return `Starting the machine... `;
+    }
+
+    brewCoffee(){
+        //complex calculation
+        return `Brewing coffee`;
+    }
+
+    pressStartButton(){
+        let msg1 = this.start();
+        let msg2 = this.brewCoffee();
+        return `${msg1} + ${msg2}`;
+    }
+}
+
+let myMachine = new CoffeeMachine();
+    // console.log(myMachine.start());
+    // console.log(myMachine.brewCoffee());
+// console.log(myMachine.pressStartButton());
+
+
+//Polymorphism
+
+ class Bird{
+    fly(){
+        return `Flying.....`;
+    }
+
+ }
+
+ class Penguin extends Bird{
+    fly(){
+        return `Penguins can't fly...`;
+    }
+ }
+
+ let bird = new Bird();
+ let penguin = new Penguin()
+//  console.log(bird.fly());
+//   console.log(penguin.fly())
+
+//static method
+class Calculator{
+    static add(a,b){
+        return a+b;
+    }
+}
+// let minCal = new Calculator();
+// console.log(minCal.add(2,3));
+// console.log(Calculator.add(2,3));
+
+//Getter and setter
+
+class Employee{
+    #salary;
+    constructor(name,salary){
+        this.name = name;
+        this._salary =  salary;
+    }
+
+    get salary(){
+    return `You are not allowed to see salary`; 
+}
+set salary(value){
+    if(value < 0){
+        console.error("Invalid Salary");
+    }
+    else{
+        this._salary = value;
+    }
+}
+}
+
+let emp = new Employee("Alice", 50000);
+console.log(emp._salary);
+emp.salary = -50000;
